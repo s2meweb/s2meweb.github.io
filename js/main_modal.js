9c48792a42c9df009209f7feffa0c424a76f98f8
s2me88 <s2me@s2meui-MacBook-Pro.local>
2017-08-08 16:39:47 +0900

    up

--- js/main_modal.js
+++ js/main_modal.js
@@ -6,13 +6,15 @@
 	var resume = about.find('.rebtn').find('button');
 	var moBox = $('.myresume');
 	
+	moBox.hide();
+	
 	resume.on('click', function(e){
 		
 		e.preventDefault();
 		
-		moBox.fadeIn();
 		
-		moBox.find('div').hide();
+		
+		
 		
 	})
 	
