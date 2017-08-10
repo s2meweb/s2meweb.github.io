9b77bf80dac1dbaf8a417216b3f2f9cbb1191631
s2me88 <s2me@s2meui-MacBook-Pro.local>
2017-08-11 03:23:25 +0900

    170811

--- js/main_modal.js
+++ js/main_modal.js
@@ -2,20 +2,33 @@
 
 (function($){
 	
-	var about = $('#about');
-	var resume = about.find('.rebtn').find('button');
-	var moBox = $('.myresume');
+	var resume = $('.resume');
+	var rebtn = resume.find('.rebtn').find('.btn');
+	var moBox = $('.myresume').find('a');
 	
-//	moBox.hide();
-	
-	resume.on('click', function(e){
+	rebtn.on('click', function(e){
 		
 		e.preventDefault();
 		
+		var mobtn = $(this).index();
+
+    moBox.fadeIn();
+   
+    moBox.eq(mobtn).fadeIn();
 		
-		
-		
-		
-	})
+		moBox.prepend('<button type="button" class="close">닫기</button>');
+  	$('.close').css({width:'3.5em', height:'3.5em', backgroundColor:'#777',
+                   position:'absolute', top:'0.2em', right:'0.2em', zIndex:500,
+                   color:'#fff', borderRadius:'100%', fontWeight:'bold'});
+  
+  var close = $('.close');
+ 	Close.on('click', function(e){
+    e.preventDefault();
+    moBox.find('div').find('.close').remove();
+    moBox.find('div').hide();
+    moBox.hide();
+	});
+	
+	});
 	
 })(this.jQuery);
