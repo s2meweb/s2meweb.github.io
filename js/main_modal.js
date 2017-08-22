1cb0cb999df288343f27831035da363d64e63db6
s2me88 <s2me@s2meui-MacBook-Pro.local>
2017-08-23 00:01:26 +0900

    170823

--- js/main_modal.js
+++ js/main_modal.js
@@ -2,33 +2,34 @@
 
 (function($){
 	
-	var resume = $('.resume');
-	var rebtn = resume.find('.rebtn').find('.btn');
-	var moBox = $('.myresume').find('a');
+	var rebtn = $('.rebtn');
+	var mbtn = rebtn.find('li');
+	var moBox = $('.myresume');
 	
-	rebtn.on('click', function(e){
-		
-		e.preventDefault();
-		
-		var mobtn = $(this).index();
-
+  mbtn.on('click', function(e){
+    // a의 기본 이벤트 제거
+    e.preventDefault();
+    // 클릭한 li의 순서 확인
+    // .eq() : 몇번째 선택  ,    .index() : 몇번째인지 확인
+    var btnLi = $(this).index();
+//    console.log(btnLi);  // 순서확인
+    // .modal 나타나기(배경 어둡게)
     moBox.fadeIn();
-   
-    moBox.eq(mobtn).fadeIn();
+    // .modal>div 순서에 맞는것을 나타나게하기
+    moBox.find(HTMLImageElement).eq(btnLi).fadeIn();
 		
 		moBox.prepend('<button type="button" class="close">닫기</button>');
   	$('.close').css({width:'3.5em', height:'3.5em', backgroundColor:'#777',
-                   position:'absolute', top:'0.2em', right:'0.2em', zIndex:500,
+                   position:'absolute' , top:'0.2em', right:'0.2em', zIndex:500,
                    color:'#fff', borderRadius:'100%', fontWeight:'bold'});
   
   var close = $('.close');
- 	Close.on('click', function(e){
+ 	close.on('click', function(e){
     e.preventDefault();
-    moBox.find('div').find('.close').remove();
-    moBox.find('div').hide();
+    moBox.find('.close').remove();
     moBox.hide();
 	});
 	
-	});
+});
 	
 })(this.jQuery);
