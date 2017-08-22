d7921669e2fd5d42df38511cde0bf14381c0821f
s2me88 <s2me@s2meui-MacBook-Pro.local>
2017-08-23 03:17:15 +0900

    170823_2

--- js/main_modal.js
+++ js/main_modal.js
@@ -3,24 +3,31 @@
 (function($){
 	
 	var rebtn = $('.rebtn');
-	var mbtn = rebtn.find('li');
+	var mbtn = rebtn.find('.btn');
 	var moBox = $('.myresume');
+//	var img = moBox.attr("../resume.jpg")
 	
-  mbtn.on('click', function(e){
-    // a의 기본 이벤트 제거
-    e.preventDefault();
-    // 클릭한 li의 순서 확인
-    // .eq() : 몇번째 선택  ,    .index() : 몇번째인지 확인
-    var btnLi = $(this).index();
-//    console.log(btnLi);  // 순서확인
-    // .modal 나타나기(배경 어둡게)
-    moBox.fadeIn();
-    // .modal>div 순서에 맞는것을 나타나게하기
-    moBox.find(HTMLImageElement).eq(btnLi).fadeIn();
+	mbtn.addClass('active');
+	moBox.addClass('active_box');
 		
-		moBox.prepend('<button type="button" class="close">닫기</button>');
+	mbtn.on('click', function(e){
+		e.preventDefault();
+		
+		var $this = $(this);
+		
+		mbtn.removeClass('active');
+		mbtn.addClass('active');
+		
+		moBox.removeClass('active_box');
+		moBox.addClass('active_box');
+		
+		
+
+
+	
+   	moBox.prepend('<button type="button" class="close">닫기</button>');
   	$('.close').css({width:'3.5em', height:'3.5em', backgroundColor:'#777',
-                   position:'absolute' , top:'0.2em', right:'0.2em', zIndex:500,
+                   position:'absolute' , top:'0.2em', right:'90%', zIndex:500,
                    color:'#fff', borderRadius:'100%', fontWeight:'bold'});
   
   var close = $('.close');
@@ -29,7 +36,35 @@
     moBox.find('.close').remove();
     moBox.hide();
 	});
+		
+	});
 	
-});
+	//---------------resume_modal---------------------
+	
+	
+			var tabLi = $('.list').find('li');
+      var tabInDl = $('.tab_box').children('dl');
+      
+      tabLi.eq(0).addClass('active');
+      tabInDl.eq(0).addClass('active_box');
+      
+      tabLi.on('click', function(e){
+        e.preventDefault();
+        
+        var $this = $(this); 
+        var indexLi = $this.index(); 
+        console.log(indexLi);
+        
+        tabLi.removeClass('active'); 
+        $this.addClass('active'); 
+        
+        
+         tabInDl.removeClass('active_box'); 
+        
+         tabInDl.eq(indexLi).addClass('active_box');
+        
+      });
+	
+	//---------------tab_box------------------------------
 	
 })(this.jQuery);
